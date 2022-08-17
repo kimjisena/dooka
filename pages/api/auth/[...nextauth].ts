47960af90468c34067ebd79bf85703d7087be282
Kim Jisena <pjohmaths@gmail.com>
2022-08-17 21:02:35 +0300

    set up google and email authentication

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+import { MongooseAdapter } from "../../../lib/MongooseAdapter";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER as string,
+      from: process.env.EMAIL_FROM as string,
+    }),
+  ],
+  adapter: MongooseAdapter(process.env.MONGODB_URI),
+});
